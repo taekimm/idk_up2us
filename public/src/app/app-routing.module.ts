119cd96f92ec28fa3d142512233c428926ad57d9
taekimm <taekimm@gmail.com>
2017-07-24 15:45:29 -0700

    change in architecture

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -5,17 +5,15 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { SearchComponent } from './landing/search/search.component';
 import { MapComponent } from './landing/map/map.component';
-import { LoginregComponent } from './loginreg/loginreg.component';
-import { LoginComponent } from './loginreg/login/login.component';
-import { RegComponent } from './loginreg/reg/reg.component';
+import { RegComponent } from './reg/reg.component';
 import { UserComponent } from './user/user.component';
 import { UpdateComponent } from './user/update/update.component';
 
 
 const routes: Routes = [
  { path: '', pathMatch: 'full', component: LandingComponent },
- { path: 'login', component: LoginregComponent},
- { path: 'search', component: LoginregComponent},
+ { path: 'register', component: RegComponent},
+ { path: 'search', component: SearchComponent},
  { path: 'user/:user_id', component: UpdateComponent},
 ];
 
