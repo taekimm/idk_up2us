08305760ea7a3b7155a7bf31ea790ba2e34f6b0b
taekimm <taekimm@gmail.com>
2017-10-31 14:34:35 -0700

    additions to index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-var http = require('http');
+var express = require('express');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World\n")
-}).listen(process.env.PORT)
+var app = express();
+
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public', 'dist')));
+
+require('./server/config/routes.js')(app);
+
+app.all("*", (req,res,next) => {
+    res.sendfile(path.resolve("./public/dist/index.html"))
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
